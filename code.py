4d35eb1a082ce3cea4a9e145b9aa25506a7e9272
gaurav1024 <gaurav.agr100@gmail.com>
2017-06-04 21:20:13 +0530

    Use randn instead of random from numpy.random

--- code.py
+++ code.py
@@ -4,8 +4,8 @@ class Network:
 	def __init__(self, sizes):
 		self.sizes = sizes
 		self.num_layers = len(sizes)
-		self.biases = [np.random.random(y, 1) for y in sizes[1:]]
-		self.weights = [np.random.random(y, x) for y, x in zip(sizes[1:], sizes[:-1])]
+		self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+		self.weights = [np.random.randn(y, x) for y, x in zip(sizes[1:], sizes[:-1])]
 
 def sigmoid(z):
 	return 1.0/(1.0 + np.exp(-z))
