7389b5348e8c529f922b39f01557027ee1e5dbc8
gaurav1024 <gaurav.agr100@gmail.com>
2017-06-04 20:58:06 +0530

    Add sigmoid function.

--- code.py
+++ code.py
@@ -6,3 +6,6 @@ class Network:
 		self.num_layers = len(sizes)
 		self.biases = [np.random.random(y, 1) for y in sizes[1:]]
 		self.weights = [np.random.random(y, x) for y, x in zip(sizes[1:], sizes[:-1])]
+
+def sigmoid(z):
+	return 1.0/(1.0 + np.exp(-z))
