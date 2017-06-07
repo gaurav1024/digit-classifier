b950a8907e7120b474f88d1c198648c929e4bf6f
gaurav1024 <gaurav.agr100@gmail.com>
2017-06-07 12:07:02 +0530

    Add the feedforward method in Network class.

--- code.py
+++ code.py
@@ -7,5 +7,10 @@ class Network:
 		self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
 		self.weights = [np.random.randn(y, x) for y, x in zip(sizes[1:], sizes[:-1])]
 
+	def feedforward(self, a):
+		for w, b in zip(self.weights, self.biases):
+			a = np.dot(w, a)+b
+		return a
+		
 def sigmoid(z):
 	return 1.0/(1.0 + np.exp(-z))
